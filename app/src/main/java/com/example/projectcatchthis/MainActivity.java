b90a32711f6010b4cdf7fd9da1c18dc1dbf33a01
Deandre Erving <DJErving@student.fullsail.edu>
2020-12-02 22:48:20 -0500

    HomeScreen Layout v1

--- app/src/main/java/com/example/projectcatchthis/MainActivity.java
+++ app/src/main/java/com/example/projectcatchthis/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.projectcatchthis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: testing this, but Start HOMESCREEN
+
+        //TODO: Create REMINDERS Page
+        //TODO: Create LISTS Page
+        //TODO: Create GROUPS Page
+        //TODO: Create SETTINGS Page
+        //TODO: Create NOTIFICATIONS Drop down menu
+        //TODO: Create Calendar with a color or symbol indication for the event type on its set date
+        //TODO: Create HOMESCREEN most 3 closet events section with date
     }
+
+    public void OnNotificationsClick(View v) {
+        TextView notif = findViewById(R.id.Tnotif);
+    }
+
 }
